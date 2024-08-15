942297c4213b53b3d9a176994cfa689e4cc6e001
Vladimir Nechaev <steadymight@gmail.com>
2024-08-15 19:27:16 +0300

    Update tracer

--- serving/tracing_forward_inference.py
+++ serving/tracing_forward_inference.py
@@ -1,7 +1,9 @@
+import argparse
 import logging
 import os
 from typing import Literal
 
+import pytorch_lightning as pl
 import torch
 
 from TTS.tts.configs.xtts_config import XttsConfig
@@ -10,7 +12,7 @@ from TTS.tts.models.xtts import Xtts
 logging.basicConfig(level=logging.INFO)
 
 
-class XttsWrapper(torch.nn.Module):
+class XttsWrapper(pl.LightningModule):
     def __init__(self, model):
         super().__init__()
         self.model = model
@@ -25,9 +27,10 @@ class XttsWrapper(torch.nn.Module):
 class Tracing:
     def __init__(
             self,
-            path_to_model,
-            path_to_traced_models,
-            export_format: Literal['torchscript', 'onnx'] = 'onnx'
+            path_to_model: str,
+            path_to_traced_models: str,
+            export_format: Literal['torchscript', 'onnx'] = 'onnx',
+            use_tensors: bool = False
     ):
         self.path_to_model = path_to_model
         self.path_to_traced_models = path_to_traced_models
@@ -35,17 +38,19 @@ class Tracing:
         self.config = self.get_config()
         self.model = self.get_model()
         self.load_checkpoint()
-        # self.trace_audio_input = torch.load('serving/audio_16k.pt')
-        # self.trace_text_input = torch.load('serving/text_tokens.pt')
-
-        self.trace_audio_len = 300000
-        self.trace_text_len = 100
-        self.trace_audio_input = torch.randn(
-            (1, self.trace_audio_len), dtype=torch.float32, requires_grad=False, device=self.model.device
-        )
-        self.trace_text_input = torch.randint(
-            0, 200, (1, self.trace_text_len), dtype=torch.int32, requires_grad=False, device=self.model.device
-        )
+        if use_tensors:
+            logging.info(f'Use predefined tensors for tracing instead of random tensors.')
+            self.trace_audio_input = torch.load('serving/audio_16k.pt').to(self.model.device)
+            self.trace_text_input = torch.load('serving/text_tokens.pt').to(self.model.device)
+        else:
+            self.trace_audio_len = 300000
+            self.trace_text_len = 100
+            self.trace_audio_input = torch.randn(
+                (1, self.trace_audio_len), dtype=torch.float32, requires_grad=False, device=self.model.device
+            )
+            self.trace_text_input = torch.randint(
+                0, 200, (1, self.trace_text_len), dtype=torch.int32, requires_grad=False, device=self.model.device
+            )
 
     def get_config(self):
         config = XttsConfig()
@@ -74,11 +79,18 @@ class Tracing:
         model_path = os.path.join(self.path_to_traced_models, model_name)
         if self.export_format == 'torchscript':
             model_path += '.pt'
-            traced_model = torch.jit.trace(
-                model,
-                example_kwarg_inputs=example_inputs
-            )
-            self.jit_save(traced_model, model_path)
+            if isinstance(model, pl.LightningModule):
+                model.to_torchscript(
+                    file_path=model_path,
+                    method="trace",
+                    example_inputs=tuple(example_inputs.values())
+                )
+            else:
+                traced_model = torch.jit.trace(
+                    model,
+                    example_kwarg_inputs=example_inputs
+                )
+                self.jit_save(traced_model, model_path)
         elif self.export_format == 'onnx':
             model_path += '.onnx'
             # onnx_program = torch.onnx.dynamo_export(
@@ -87,17 +99,19 @@ class Tracing:
             # )
             # self.onnx_save(onnx_program, model_path)
             torch.onnx.export(
-                model,  # model being run
-                example_inputs,  # model input (or a tuple for multiple inputs)
-                model_path,  # where to save the model (can be a file or file-like object)
+                model=model,  # model being run
+                args=tuple(example_inputs.values()),  # model input (or a tuple for multiple inputs)
+                f=model_path,  # where to save the model (can be a file or file-like object)
                 export_params=True,  # store the trained parameter weights inside the model file
-                opset_version=18,  # the ONNX version to export the model to
+                verbose=True,
+                opset_version=15,  # the ONNX version to export the model to
                 do_constant_folding=True,  # whether to execute constant folding for optimization
-                input_names=['input_1', 'input_2'],  # the model's input names
+                input_names=['input_0', 'input_1'],  # the model's input names
                 output_names=['output'],  # the model's output names
                 dynamic_axes={
-                    'input': {1: 'audio_len', 2: 'text_len'},  # variable length axes
-                    'output': {1: 'audio_len'}
+                    'input_0': {0: 'batch_size', 1: 'audio_len'},
+                    'input_1': {0: 'batch_size', 1: 'text_len'},
+                    'output': {0: 'batch_size', 1: 'audio_len'}
                 }
             )
 
@@ -116,9 +130,51 @@ class Tracing:
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Tracing XTTS model.'
+    )
+
+    parser.add_argument(
+        '--path_to_model', '-p',
+        dest='path_to_model',
+        nargs='?',
+        type=str,
+        help='Path to folder with XTTS checkpoints.',
+        default='/Users/might/Library/Application Support/tts/tts_models--multilingual--multi-dataset--xtts_v2'
+    )
+
+    parser.add_argument(
+        '--path_to_traced_models', '-s',
+        dest='path_to_traced_models',
+        nargs='?',
+        type=str,
+        help='Path to folder where traced model should be exported.',
+        default='/Users/might/Downloads/xtts_traced'
+    )
+
+    parser.add_argument(
+        '--export_format', '-e',
+        dest='export_format',
+        nargs='?',
+        type=str,
+        choices=['torchscript', 'onnx'],
+        help='Export format.',
+        default='onnx'
+    )
+
+    parser.add_argument(
+        '--use_tensors',
+        dest='use_tensors',
+        help='If specified, then use predefined tensors instead of random input tensors.',
+        action='store_true'
+    )
+
+    args = parser.parse_args()
+
     tracer = Tracing(
-        path_to_model='/Users/might/Library/Application Support/tts/tts_models--multilingual--multi-dataset--xtts_v2',
-        path_to_traced_models='/Users/might/Downloads/xtts_traced',
-        export_format='onnx'
+        path_to_model=args.path_to_model,
+        path_to_traced_models=args.path_to_traced_models,
+        export_format=args.export_format,
+        use_tensors=args.use_tensors
     )
     tracer.trace()
